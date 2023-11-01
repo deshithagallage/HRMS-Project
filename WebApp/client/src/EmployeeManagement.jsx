e1223899147eaf1eab035d86c90a65722c2fbf54
deshitha-g <deshithagallage623@gmail.com>
2023-11-01 22:33:55 +0530

    Minor issues fixed

--- WebApp/client/src/EmployeeManagement.jsx
+++ WebApp/client/src/EmployeeManagement.jsx
@@ -3,7 +3,6 @@ import Axios from 'axios';
 import { useNavigate, useParams,NavLink, Outlet } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
 import './styles/EmployeeManagement.css'; // Import the CSS file
-import './styles/PageHR.css'; // Import the CSS file
 import NavBar from './Navbar';
 
 function handleLogout() {
@@ -117,89 +116,89 @@ function EmployeeManagement() {
           </li>
         </ul>
       </div>
-    <div className="content">
-    <NavBar text="Employee Management"/>      
+      <div className="content">
+      <NavBar text="Employee Management"/>      
 
-      <div className="d-flex justify-content-between">
-        <button type="button" className="btn btn-success my-3" style={{ margin: '30px' }} onClick={handleAddEmployee}>
-          Add New Employee
-        </button>
-      
-        <div className="search-bar">
-          <input
-            type="text"
-            id="search-bar"
-            placeholder="Search employees"
-            value={searchTerm}
-            onChange={handleSearchChange}
+        <div className="d-flex justify-content-between">
+          <button type="button" className="btn btn-success my-3" style={{ margin: '30px' }} onClick={handleAddEmployee}>
+            Add New Employee
+          </button>
+        
+          <div className="search-bar">
+            <input
+              type="text"
+              id="search-bar"
+              placeholder="Search employees"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+          </div>
+        </div>
+        <div className="table-container" >
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Employee ID</th>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Job Title</th>
+                <th>Email</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {displayedEmployees.map((employee) => (
+                <tr key={employee.Employee_ID}>
+                  <td>{employee.Employee_ID}</td>
+                  <td>{employee.First_Name + " " + employee.Last_Name}</td>
+                  <td>{employee.Gender}</td>
+                  <td>{employee.Job_Title}</td>
+                  <td>{employee.Email}</td>
+                  <td>
+                    <div style={{margin: "0px"}} className="d-flex custom-button-container">
+                      <div style={{margin: "0px"}} className="mx-1">
+                        <button type="button" className="btn btn-success" onClick={
+                          () => {
+                            const id_to_view = employee.Employee_ID;
+                            navigate(`/PageHR/${id_to_transfer}/EmployeeManagement/ViewEmployee/${id_to_view}`)
+                          }
+                        }>
+                          View
+                        </button>
+                      </div>
+                      <div style={{margin: "0px"}} className="mx-1">
+                        <button type="button" className="btn btn-warning" onClick={
+                          () => {
+                            const id_to_edit = employee.Employee_ID;
+                            navigate(`/PageHR/${id_to_transfer}/EmployeeManagement/EditEmployee/${id_to_edit}`)
+                          }
+                        }>
+                          Edit
+                        </button>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+          
+        <div className="pagination-container">
+          <ReactPaginate
+            previousLabel={customButtonLabel('Previous')}
+            nextLabel={customButtonLabel('Next')}
+            breakLabel={<span>...</span>}
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            containerClassName="pagination"
+            subContainerClassName="pages pagination"
+            activeClassName="active"
           />
         </div>
       </div>
-      <div className="table-container" >
-        <table className="table table-striped table-bordered">
-          <thead>
-            <tr>
-              <th>Employee ID</th>
-              <th>Name</th>
-              <th>Gender</th>
-              <th>Job Title</th>
-              <th>Email</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {displayedEmployees.map((employee) => (
-              <tr key={employee.Employee_ID}>
-                <td>{employee.Employee_ID}</td>
-                <td>{employee.First_Name + " " + employee.Last_Name}</td>
-                <td>{employee.Gender}</td>
-                <td>{employee.Job_Title}</td>
-                <td>{employee.Email}</td>
-                <td>
-                  <div style={{margin: "0px"}} className="d-flex custom-button-container">
-                    <div style={{margin: "0px"}} className="mx-1">
-                      <button type="button" className="btn btn-success" onClick={
-                        () => {
-                          const id_to_view = employee.Employee_ID;
-                          navigate(`/PageHR/${id_to_transfer}/EmployeeManagement/ViewEmployee/${id_to_view}`)
-                        }
-                      }>
-                        View
-                      </button>
-                    </div>
-                    <div style={{margin: "0px"}} className="mx-1">
-                      <button type="button" className="btn btn-warning" onClick={
-                        () => {
-                          const id_to_edit = employee.Employee_ID;
-                          navigate(`/PageHR/${id_to_transfer}/EmployeeManagement/EditEmployee/${id_to_edit}`)
-                        }
-                      }>
-                        Edit
-                      </button>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-        
-      <div className="pagination-container">
-        <ReactPaginate
-          previousLabel={customButtonLabel('Previous')}
-          nextLabel={customButtonLabel('Next')}
-          breakLabel={<span>...</span>}
-          pageCount={pageCount}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={handlePageClick}
-          containerClassName="pagination"
-          subContainerClassName="pages pagination"
-          activeClassName="active"
-        />
-      </div>
-    </div>
     </div>
   );
 }
