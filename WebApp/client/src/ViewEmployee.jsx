d1ed6035232a97b9a834c9c0769a0a86465b1569
deshitha-g <deshithagallage623@gmail.com>
2023-11-03 10:02:58 +0530

    Bug fixes

--- WebApp/client/src/ViewEmployee.jsx
+++ WebApp/client/src/ViewEmployee.jsx
@@ -43,7 +43,6 @@ function ViewEmployee() {
 
   return (
     <div style={{marginLeft: '20px', marginRight: '20px'}}>
-      <h1>This is the view employee page</h1>
       <div className="d-flex flex-column align-items-center" style={{
         border: '2px solid black',
         padding: '20px',
